524a54e70eefab1060d7db87451bea9662f8e65d
GpeyB <gabrielpeytral@comunidad.unam.mx>
2021-10-15 18:10:51 -0500

    ps a ver si jala

--- Puntos/Punto.cpp
+++ Puntos/Punto.cpp
@@ -1,6 +1,44 @@
-#include <iostream>
+//
+// Created by gapeb on 10/14/2021.
+//
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+
+#include <iostream>
+#include <array>
+#include "Punto.h"
+#include <math.h>
+
+Punto2D::Punto2D() {
+
+}
+
+void Punto2D::SetPosicion(float x_, float y_){
+    x = x_;
+    y = y_;
+}
+
+void Punto2D:: Trasladar(float trax, float tray){
+    x += trax;
+    y += tray;
+}
+
+void Punto2D:: RotarRespectoAlOrigen(float a){
+    a *= 3.14159/ 180.0;
+    float kp;
+    kp = x * cos (a) -  y * sin(a);
+    y = x * sin(a) + y * cos(a);
+    x = kp;
+}
+void Punto2D:: Escalar(float esx, float esy){
+    x *= esx;
+    y *= esy;
+}
+
+float Punto2D::GetX() {
+    return x;
+}
+
+float Punto2D::GetY() {
+    return y;
 }
--- Puntos/Punto.h
+++ Puntos/Punto.h
@@ -5,4 +5,25 @@
 #ifndef PUNTOS_PUNTO_H
 #define PUNTOS_PUNTO_H
 
+class Punto2D {
+public:
+    Punto2D();
+
+    //Métodos
+    void SetPosicion(float x_, float y_);
+    void Trasladar(float trax, float tray);
+    void RotarRespectoAlOrigen(float a);
+    void Escalar(float esx, float esy);
+
+
+    float GetX ();
+    float GetY ();
+
+
+
+private:
+    float x,y; // Variables de clase
+
+};
+
 #endif //PUNTOS_PUNTO_H

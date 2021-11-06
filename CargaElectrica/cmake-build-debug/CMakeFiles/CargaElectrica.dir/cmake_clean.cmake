89840bd3b6e74b44ff500dbd5c9efc936c73db8d
GPeyB <89151612+GPeyB@users.noreply.github.com>
2021-11-05 22:45:12 -0600

    Add files via upload

---
+++ CargaElectrica/cmake-build-debug/CMakeFiles/CargaElectrica.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CargaElectrica.dir/Grupo.cpp.obj"
+  "CargaElectrica.exe"
+  "CargaElectrica.exe.manifest"
+  "CargaElectrica.pdb"
+  "libCargaElectrica.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CargaElectrica.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
